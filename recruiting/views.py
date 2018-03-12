cba2083c696f4bb6085f34c4b97fe02f8cd95025
realchief <stryokka@gmail.com>
2018-03-12 02:35:07 +0100

    get title and location

--- recruiting/views.py
+++ recruiting/views.py
@@ -9,10 +9,11 @@ from rest_framework import viewsets
 from recruiting.serializers import UserSerializer, GroupSerializer
 from rest_framework.views import APIView
 from recruiting.serializers import UserSerializer
+import requests
 from rest_framework.parsers import FormParser
+from lxml import html
 
 # Create your views here.
-
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -36,8 +37,13 @@ class Parser(APIView):
         locationkey = request.path.split('/')[-1]
         start_requests = 'https://www.trainee.de/traineestellen/?search_job%5Bquery%5D=&search_job%5Blocation%' \
                          '5D=' + locationkey + '&search_job%5Bdistance%5D=25'
+        r = requests.get(start_requests)
+        dom = html.fromstring(r.text)
+        for index in range(0, 20):
+            title = str(dom.xpath('//div[@id="jobs"]//ul/li')[index].xpath('.//h4//text()')[0])
+            company_name = str(dom.xpath('//div[@id="jobs"]//ul/li')[index].xpath('.//span//text()')[0])
+            image_list = str(dom.xpath('//div[@id="jobs"]//ul/li')[index].xpath('.//img//@src')[0])
 
-        print(locationkey)
-
+        print(start_requests)
 
 
